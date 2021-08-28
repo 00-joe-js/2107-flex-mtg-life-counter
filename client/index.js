1c8fbe1926d2ff36a4363d9419c766558396d7a8
Joe Alves <joseph.michael.alves@gmail.com>
2021-08-28 12:48:59 -0400

    End of lecture

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import store from "./redux-store";
 
+import {healPlayer1, damagePlayer1, healPlayer2, damagePlayer2} from "./redux-store";
+
 window.addEventListener("DOMContentLoaded", () => {
 
   const player1 = document.querySelector("#player-1-score");
@@ -9,29 +11,32 @@ window.addEventListener("DOMContentLoaded", () => {
   const player1Down = player1.querySelector("h4.down-arrow");
 
   player1Up.addEventListener("click", () => {
-    store.dispatch({ type: "HEAL_PLAYER_1" });
+    // store.dispatch({ type: "HEAL_PLAYER_1" });
+    store.dispatch(healPlayer1());
   });
   player1Down.addEventListener("click", () => {
-    store.dispatch({ type: "DAMAGE_PLAYER_1" });
+    store.dispatch(damagePlayer1());
   });
 
   const player2Up = player2.querySelector("h4:first-child");
   const player2Down = player2.querySelector("h4.down-arrow");
 
   player2Up.addEventListener("click", () => {
-    store.dispatch({ type: "HEAL_PLAYER_2" });
+    store.dispatch(healPlayer2());
   });
   player2Down.addEventListener("click", () => {
-    store.dispatch({ type: "DAMAGE_PLAYER_2" });
+    store.dispatch(damagePlayer2());
   });
 
   const player1Score = player1.querySelector("h3");
   const player2Score = player2.querySelector("h3");
 
+  // Call this function when the Redux store state has updated.
   store.subscribe(() => {
     const state = store.getState();
     player1Score.innerText = state.player1Health;
     player2Score.innerText = state.player2Health;
   });
+
 
 });
--- client/redux-store.js
+++ client/redux-store.js
@@ -5,35 +5,62 @@ const initialState = {
     player2Health: 20
 };
 
+// Action constants.
+const HEAL_PLAYER_1 = "HEAL_PLAYER_1";
+const HEAL_PLAYER_2 = "HEAL_PLAYER_2";
+const DAMAGE_PLAYER_1 = "DAMAGE_PLAYER_1";
+const DAMAGE_PLAYER_2 = "DAMAGE_PLAYER_2";
+
+// Action creator
+// This function when invoked "creates" an action object
+// for anywhere in your application to dispatch.
+// This is so that the other parts of your application
+// DO NOT NEED TO KNOW how to dispatch an instruction to the store.
+export const healPlayer1 = () => {
+    return { type: HEAL_PLAYER_1 };
+};
+export const damagePlayer1 = () => {
+    return { type: DAMAGE_PLAYER_1 };
+};
+export const healPlayer2 = () => {
+    return { type: HEAL_PLAYER_2 };
+};
+export const damagePlayer2 = () => {
+    return { type: DAMAGE_PLAYER_2 };
+};
+
+// When an action is dispatched (store.dispatch)
+// take in the current state of the store and the action object
+// and produce the next state.
 const reducer = (currentState = initialState, action) => {
 
-    let newState = currentState;
-
-    if (action.type === "DAMAGE_PLAYER_1") {
-        newState = {
+    if (action.type === DAMAGE_PLAYER_1) {
+        return {
             player1Health: currentState.player1Health - 1,
             player2Health: currentState.player2Health
         };
-    } else if (action.type === "DAMAGE_PLAYER_2") {
-        newState = {
+    } else if (action.type === DAMAGE_PLAYER_2) {
+        return {
             player1Health: currentState.player1Health,
             player2Health: currentState.player2Health - 1
         };
-    } else if (action.type === "HEAL_PLAYER_1") {
-        newState = {
+    } else if (action.type === HEAL_PLAYER_1) {
+        return {
             player1Health: currentState.player1Health + 1,
             player2Health: currentState.player2Health
         };
-    } else if (action.type === "HEAL_PLAYER_2") {
-        newState = {
+    } else if (action.type === HEAL_PLAYER_2) {
+        return {
             player1Health: currentState.player1Health,
             player2Health: currentState.player2Health + 1
         };
     }
 
-    return newState;
+    return currentState;
 
 };
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 export default store;
+
+window.store = store;
